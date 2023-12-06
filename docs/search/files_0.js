4ab9b0c8cf5812edb35ecbc5dd65572342dd5bca
Gust4vo73 <151526988+Gust4vo73@users.noreply.github.com>
2023-12-06 09:39:39 -0300

    Add files via upload

---
+++ docs/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['salário_20bruto_20_2b_20salário_20liquido_2ec_0',['salário bruto + salário liquido.c',['../sal_xC3_xA1rio_01bruto_01_09_01sal_xC3_xA1rio_01liquido_8c.html',1,'']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['main_0',['main',['../sal_xC3_xA1rio_01bruto_01_09_01sal_xC3_xA1rio_01liquido_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'salário bruto + salário liquido.c']]]
+];
